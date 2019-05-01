2ac44a413345af445ca2feb2a3110830df38a085
tylersingh <tylersingh@users.github.com>
2019-04-30 20:15:25 -0400

    Update

---
+++ assets/javascript/face.js
@@ -0,0 +1,13 @@
+//this app will be able to take live photos and upload from local storage
+
+//this app will be able to detect user emotion from the image uploaded
+
+
+
+
+
+
+//api for Face++ rZislq8kNQ8TD5wQ1iEtgEDNVY5KGJZK
+//api for Face (7 day trial) Endpoint: https://westcentralus.api.cognitive.microsoft.com/face/v1.0
+//Key 1: 54f3e0d43e1042329079b7f39fd8019e
+//Key 2: 24d16bd0a1e4401faeb99469c6ed3f07
