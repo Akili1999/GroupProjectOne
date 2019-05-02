25edd363eaae703dc123d168765aeef378350cd5
Akili Mcmackin <47020399+Akili1999@users.noreply.github.com>
2019-05-02 19:41:00 -0400

    Update spotify.js

---
+++ assets/javascript/spotify.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+	function authorize() {
+		var queryURL = "https://cors-anywhere.herokuapp.com/https://accounts.spotify.com/authorize?client_id=404cd5ddbefc43d09fed23f29cd77530" +
+				"&response_type=token&redirect_uri=https:%2F%2Fakili1999.github.io%2FGroupProjectOne%2F"
+		$.ajax({
+			url: queryURL,
+			method: 'GET',
+		}).then(function(response) {
+			console.log(response);
+			
+		})
+	}
+  
+  	app.init = function() {
+		$('form').on('submit', function(e) {
+			e.preventDefault();
+			authorize();
+      		});
+	
+	}
+ });
